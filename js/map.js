9b1af2e86a8118aad7dd6f1c8dbd494f0054b81f
Robert Matteson <rmatteso@gmail.com>
2017-03-29 21:38:08 -0400

    incremental

--- js/map.js
+++ js/map.js
@@ -5,7 +5,7 @@ var infowindow;
 var markers = [];
 
 function initMap() {
-    var zoom = 16;
+    var zoom = 13;
     var defaultLoc =    {
         // Chapel Hill, NC
         lat: 35.880,
@@ -52,14 +52,18 @@ function initMap() {
     
 }
 
+// TODO: correlate with http://developer.tmsapi.com/docs/data_v1_1/movies/Movie_showtimes API for showtimes
+
+
+
 function addPlaces(position)    {
     infowindow = new google.maps.InfoWindow();
     var service = new google.maps.places.PlacesService(map);
     
     service.nearbySearch({
         location: position,
-        radius: 500,
-        type: ['restaurant']
+        radius: 10000,
+        type: ['movie_theater']
     }, callback);
     
     function callback(results, status) {
@@ -68,7 +72,17 @@ function addPlaces(position)    {
                 createMarker(results[i]);
             }
         }
-    }
+    };
+    
+    function getDetails(placeId)    {
+        var service = new google.maps.places.PlacesService(map);
+
+        service.getDetails({
+            placeId: placeId
+        }, function(results, status){
+            console.log(results);
+        });
+    };
     
     function createMarker(place) {
         var placeLoc = place.geometry.location;
@@ -79,11 +93,17 @@ function addPlaces(position)    {
             animation: google.maps.Animation.DROP,
             index: markers.length, // hopefully this will help with the show/hide
         });
+        
+        console.log(place);
+        var details = getDetails(place.place_id);
 
         marker.addListener('click', function() {
             // add additional stuff here
             infowindow.setContent(
-                place.name
+                '<strong>'+place.name+'</strong><br />'+
+                //'<span>'+place.place_id+'</span>'
+                '<span>'+details.formatted_address+'</span><br />'+
+                '<span>'+details.formatted_phone_number+'</span>'
             );
             
             infowindow.open(map, this);
@@ -115,3 +135,135 @@ function bindKnockout() {
 
     ko.applyBindings(new viewModel());
 }
+
+// NYT API
+//
+// Key: f0ba834b7431421e8971e70bda9a08bc
+
+function getNYT()   {
+    var url = "https://api.nytimes.com/svc/movies/v2/reviews/search.json";
+    url += '?' + $.param({
+      'api-key': "f0ba834b7431421e8971e70bda9a08bc"
+    });
+    $.ajax({
+        url: url,
+        method: 'GET',
+    }).done(function(result) {
+        console.log(result);
+    }).fail(function(err) {
+        throw err;
+    });
+};
+
+// YELP FUSION API
+//
+// App Id: ce9jQ8AOQ8_YI_oLw-holQ
+// App Secret: q6fEh4OFhrDXiXniITHAhoABFf6uVkud3LytSs3ywjSaMtvel2raOT2JkcBZ5qyg
+
+/*
+var oauth = OAuth({
+    consumer: {
+        key: 'PypE7f5VMTQ-n2mdSHSvVQ',
+        secret: 'RaX8bj02RpHrNBb_ymfW3BuBt5M'
+    },
+    signature_method: 'HMAC-SHA1',
+    hash_function: function(base_string, key) {
+        return CryptoJS.HmacSHA1(base_string, key).toString(CryptoJS.enc.Base64);
+    }
+});
+
+var request_data = {
+    url: 'https://api.yelp.com/v2/search?term=food&location=San+Francisco&format=jsonp?callback=?',
+    method: 'GET'
+};
+
+var token = {
+    key: '2hCEZOnNqQl_GCXP2YuprTQp3syrsT8_',
+    secret: 'AhYqgFgxLHTQxejxD2wud1UEFkk'
+};
+
+$.ajax({
+    url: request_data.url,
+    type: request_data.method,
+    data: oauth.authorize(request_data, token),
+    dataType: 'jsonp',
+    cache: true,
+    success: function(data) {
+        console.log('success');
+    },
+    error: function(data) {
+        console.log(data);
+    }
+});
+
+*/
+/*
+
+//  YELP API v2.0
+//  Consumer Key	PypE7f5VMTQ-n2mdSHSvVQ
+//  Consumer Secret	RaX8bj02RpHrNBb_ymfW3BuBt5M
+//  Token	2hCEZOnNqQl_GCXP2YuprTQp3syrsT8_
+//  Token Secret	AhYqgFgxLHTQxejxD2wud1UEFkk
+
+var oauth = OAuth({
+    consumer: {
+        key: 'PypE7f5VMTQ-n2mdSHSvVQ',
+        secret: 'RaX8bj02RpHrNBb_ymfW3BuBt5M'
+    },
+    signature_method: 'HMAC-SHA1',
+    hash_function: function(base_string, key) {
+        return CryptoJS.HmacSHA1(base_string, key).toString(CryptoJS.enc.Base64);
+    }
+});
+
+var request_data = {
+    url: 'https://api.yelp.com/v2/search?term=food&location=San+Francisco&format=jsonp?callback=?',
+    method: 'GET'
+};
+
+var token = {
+    key: '2hCEZOnNqQl_GCXP2YuprTQp3syrsT8_',
+    secret: 'AhYqgFgxLHTQxejxD2wud1UEFkk'
+};
+
+$.ajax({
+    url: request_data.url,
+    type: request_data.method,
+    data: oauth.authorize(request_data, token),
+    dataType: 'jsonp',
+    cache: true,
+    success: function(data) {
+        console.log('success');
+    },
+    error: function(data) {
+        console.log(data);
+    }
+});
+
+*/
+
+/*
+
+Flickr info
+
+udacity-neighborhood-project
+Key:
+12a0460908d2427cf12de4bf92e9bc5f
+
+Secret:
+876228c59af5be4d
+
+$.get('https://api.flickr.com/services/rest/?&method=flickr.people.getPublicPhotos&format=json&api_key=6f93d9bd5fef5831ec592f0b527fdeff&user_id=9395899@N08')
+
+var url = 'https://api.flickr.com/services/rest/?';
+var method = 'flickr.people.getPublicPhotos';
+
+var parameters = [];
+parameters.push('format=json');
+parameters.push('api_key=12a0460908d2427cf12de4bf92e9bc5f');
+parameters.push('user_id=876228c59af5be4d');
+parameters.join('');
+
+var request = url+method+parameters.join('');
+
+*/
